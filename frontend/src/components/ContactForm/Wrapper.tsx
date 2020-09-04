a476428b316c860c92548e4c7e4e4f0b598cc96f
Vlatko Trajkov <trajkovvlatko@gmail.com>
2020-09-04 14:54:35 +0200

    Update useFetch to useRequest

--- frontend/src/components/ContactForm/Wrapper.tsx
+++ frontend/src/components/ContactForm/Wrapper.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import './Wrapper.scss';
 import Form from './Form';
+import {useRequest} from 'hooks/useRequest';
 
 type IForm = {
   [x: string]: any;
 };
 
 function Wrapper() {
-  const onSubmit = (data: IForm) => {
-    console.log(data);
+  const {data, sendRequest} = useRequest();
+  const url = 'http://localhost:3000/data.json';
+
+  const onSubmit = async (values: IForm) => {
+    const formValues = new FormData();
+    formValues.append('name', values.name);
+    formValues.append('message', values.message);
+    await sendRequest(url, 'POST', null, formValues);
   };
 
   return (
     <div>
       <h2>Contact form</h2>
+      {data.error && <div>An error occured.</div>}
+      {data.loading && <div>Please wait...</div>}
       <Form onSubmit={onSubmit} />
     </div>
   );
