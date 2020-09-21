09973c2d1c6b98e9d452da1544fa51d1cc364492
Vlatko Trajkov <trajkovvlatko@gmail.com>
2020-09-21 10:14:51 +0200

    POC timeseries forecast

--- backend/src/resolver/Chart.ts
+++ backend/src/resolver/Chart.ts
@@ -1,4 +1,11 @@
-import {Resolver, Query, UseMiddleware, Ctx} from 'type-graphql';
+import {
+  Resolver,
+  Query,
+  UseMiddleware,
+  Ctx,
+  ObjectType,
+  Field,
+} from 'type-graphql';
 import Chart from '../entity/Chart';
 import Exercise from '../entity/Exercise';
 import GlucoseLevel from '../entity/GlucoseLevel';
@@ -6,9 +13,58 @@ import Meal from '../entity/Meal';
 import Weight from '../entity/Weight';
 import withUser from '../middlewares/withUser';
 import {TContext} from '../types/TContext';
+import timeseries from 'timeseries-analysis';
+
+@ObjectType()
+class ChartResponse {
+  @Field(() => Number)
+  a: number;
+}
 
 @Resolver(Exercise)
 export default class ChartResolver {
+  @Query(() => ChartResponse, {nullable: true})
+  blah(): ChartResponse {
+    const data = [
+      ['2012-01-01', 1],
+      ['2012-01-02', 1],
+      ['2012-01-03', 2],
+      ['2012-01-04', 2],
+      ['2012-01-05', 3],
+      ['2012-01-06', 3],
+      ['2012-01-07', 4],
+      ['2012-01-08', 4],
+      ['2012-01-09', 5],
+      ['2012-01-10', 5],
+      ['2012-01-11', 6],
+      ['2012-01-12', 6],
+      ['2012-01-13', 7],
+      ['2012-01-14', 7],
+    ];
+
+    for (let i = 0; i < 5; i++) {
+      const t = new timeseries.main(data);
+      const coeffs = t.ARMaxEntropy({
+        data: t.data,
+      });
+      const length = data.length - 1;
+
+      let forecast = 0;
+      for (let j = 0; j < coeffs.length; j++) {
+        forecast -= t.data[length - j][1] * coeffs[j];
+      }
+      const nextIndex = data.length;
+      const nextDate = nextIndex + 1;
+      data[nextIndex] = [`2012-01-${nextDate}`, Math.round(forecast)];
+    }
+
+    console.log('forecast', data);
+
+    return {
+      a: 1,
+    };
+  }
+
   // Example:
   // query Chart {
   //   charts {
